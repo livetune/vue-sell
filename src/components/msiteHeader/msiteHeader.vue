1d803a69ebf4709fd861fc859a3322d4faa131b6
livetune <243443394@qq.com>
2018-02-25 20:07:50 +0800

    ver 0.0

---
+++ src/components/msiteHeader/msiteHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header-wrapper">
+    <div class="search-wrapper"></div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'search-header'
+  };
+</script>
+
+<style lang="stylus">
+  .header-wrapper
+    .search-wrapper
+      color #cccccc
+</style>
--- src/page/msite/msite.vue
+++ src/page/msite/msite.vue
@@ -1,5 +1,13 @@
 <template>
-<div class="msite">msite</div>
+<div class="msite">msite
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="position:absolute;width:0;height:0">
+    <defs>
+      <symbol viewBox="0 0 26 31" id="location" width="100%" height="100%"><path fill="#FFF" fill-rule="evenodd" d="M22.116 22.601c-2.329 2.804-7.669 7.827-7.669 7.827-.799.762-2.094.763-2.897-.008 0 0-5.26-4.97-7.643-7.796C1.524 19.8 0 16.89 0 13.194 0 5.908 5.82 0 13 0s13 5.907 13 13.195c0 3.682-1.554 6.602-3.884 9.406zM18 13a5 5 0 1 0-10 0 5 5 0 0 0 10 0z"></path>
+      </symbol>
+      <symbol viewBox="0 0 14 8" id="arrow" width="100%" height="100%"><path fill="#FFF" fill-rule="evenodd" d="M5.588 6.588c.78.78 2.04.784 2.824 0l5.176-5.176c.78-.78.517-1.412-.582-1.412H.994C-.107 0-.372.628.412 1.412l5.176 5.176z"></path></symbol>
+    </defs>
+  </svg>
+</div>
 </template>
 
 <script>
